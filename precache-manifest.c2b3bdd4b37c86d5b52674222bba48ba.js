17dca6be4064291b41d431bacf6037f34507b787
A-Game <javascript@adamheldring.com>
2020-04-22 12:16:47 +0200

    Updates

---
+++ precache-manifest.c2b3bdd4b37c86d5b52674222bba48ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "47066e81bd000ae45ef4",
+    "url": "/BitCoinTracker/static/css/main.1da74963.chunk.css"
+  },
+  {
+    "revision": "47066e81bd000ae45ef4",
+    "url": "/BitCoinTracker/static/js/main.47066e81.chunk.js"
+  },
+  {
+    "revision": "1ae73f53ebc124c49529",
+    "url": "/BitCoinTracker/static/js/1.1ae73f53.chunk.js"
+  },
+  {
+    "revision": "37f71a91076086df524a",
+    "url": "/BitCoinTracker/static/js/runtime~main.37f71a91.js"
+  },
+  {
+    "revision": "9093b2cc4c32f32a17fd831747682778",
+    "url": "/BitCoinTracker/static/media/dollar.9093b2cc.jpg"
+  },
+  {
+    "revision": "72c6fbfc252cd89b070141fe0339a905",
+    "url": "/BitCoinTracker/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/BitCoinTracker/precache-manifest.d241453dfb90138260402ae8582c0d66.js"
+  "/BitCoinTracker/precache-manifest.c2b3bdd4b37c86d5b52674222bba48ba.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/BitCoinTracker/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
